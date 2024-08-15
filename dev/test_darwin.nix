285fd27c321e673dfc178864582c3475dd69371f
wind-02 <wind.0203@proton.me>
2024-08-15 11:36:04 +0900

    Update test_darwin.nix

--- dev/test_darwin.nix
+++ dev/test_darwin.nix
@@ -1,5 +1,5 @@
 { pkgs }:
 
 pkgs.darwin.mkShell {
-  buildInputs = pkgs.darwin.default.defaultPkgs ++ pkgs.darwin.default.utilPkgs ++ pkgs.darwin.dev.goPkgs;
+  buildInputs = pkgs.darwin.default.defaultPkgs;
 }
